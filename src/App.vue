7026df31b480e528f1cc46e2c4753e9428e5a3b3
BillyZou <yz3352@nyu.edu>
2020-01-08 18:07:52 +0800

    init: initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <img src="./logo.png">
+  <HelloWorld />
+  <button @click="inc">Clicked {{ count }} times.</button>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  components: {
+    HelloWorld,
+  },
+  setup() {
+    const count = ref(0)
+    const inc = () => {
+      count.value++
+    }
+
+    return {
+      count,
+      inc
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {
+  width: 200px;
+}
+h1 {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Hello Vue3!</h1>
+</template>
+
+<script lang="ts">
+export default {}
+</script>
